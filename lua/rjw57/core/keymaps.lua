9fe2702f6a4d95caed892daba4ca8c6217d8f22c
Rich Wareham <rich.wareham@bbc.co.uk>
2022-11-08 09:39:03 +0000

    add git beymap

--- lua/rjw57/core/keymaps.lua
+++ lua/rjw57/core/keymaps.lua
@@ -3,3 +3,7 @@ local keymap = vim.keymap
 
 -- nvim-tree configuration
 keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
+
+-- git configuration
+keymap.set("n", "<leader>gp", ":Git push")
+keymap.set("n", "<leader>gP", ":Git push --force")
