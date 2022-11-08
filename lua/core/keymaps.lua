4fbe8168cbfc7d95e80feaf7898ef87b4dac4fc7
Rich Wareham <rich@richwareham.com>
2022-11-08 21:48:00 +0000

    fix indent

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -11,20 +11,20 @@ keymap.set("n", "<leader>gP", ":Git push --force<CR>")
 -- telescope
 local status, telescope_builtin = pcall(require, "telescope.builtin")
 if status then
-	keymap.set("n", "<C-p>", telescope_builtin.find_files, {})
-	keymap.set("n", "<leader>ff", telescope_builtin.find_files, {})
-	keymap.set("n", "<leader>fg", telescope_builtin.live_grep, {})
-	keymap.set("n", "<leader>fb", telescope_builtin.buffers, {})
-	keymap.set("n", "<leader>fh", telescope_builtin.help_tags, {})
-	keymap.set("n", "<leader>ft", telescope_builtin.treesitter, {})
+  keymap.set("n", "<C-p>", telescope_builtin.find_files, {})
+  keymap.set("n", "<leader>ff", telescope_builtin.find_files, {})
+  keymap.set("n", "<leader>fg", telescope_builtin.live_grep, {})
+  keymap.set("n", "<leader>fb", telescope_builtin.buffers, {})
+  keymap.set("n", "<leader>fh", telescope_builtin.help_tags, {})
+  keymap.set("n", "<leader>ft", telescope_builtin.treesitter, {})
 end
 
 -- diagnostics
 vim.api.nvim_set_keymap(
-	"n",
-	"<leader>do",
-	"<cmd>lua vim.diagnostic.open_float()<CR>",
-	{ noremap = true, silent = true }
+  "n",
+  "<leader>do",
+  "<cmd>lua vim.diagnostic.open_float()<CR>",
+  { noremap = true, silent = true }
 )
 vim.api.nvim_set_keymap("n", "<leader>d[", "<cmd>lua vim.diagnostic.goto_prev()<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>d]", "<cmd>lua vim.diagnostic.goto_next()<CR>", { noremap = true, silent = true })
@@ -32,7 +32,7 @@ vim.api.nvim_set_keymap("n", "<leader>dd", "<cmd>Telescope diagnostics<CR>", { n
 
 -- LSP
 vim.api.nvim_create_autocmd("LspAttach", {
-	callback = function(args)
-		vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = args.buf })
-	end,
+  callback = function(args)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = args.buf })
+  end,
 })
