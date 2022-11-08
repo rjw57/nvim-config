5b47df738b7e4bfe8398b77f6d1d0450ead28b20
Rich Wareham <rich@richwareham.com>
2022-11-08 21:11:25 +0000

    add deno language server

--- lua/rjw57/plugins/lsp/mason.lua
+++ lua/rjw57/plugins/lsp/mason.lua
@@ -17,14 +17,15 @@ mason_lspconfig.setup({
   -- list of servers for mason to install
   ensure_installed = {
     "cssls",
+    "denols",
     "emmet_ls",
+    "eslint",
     "html",
+    "luau_lsp",
     "pylsp",
     "sumneko_lua",
     "tailwindcss",
     "tsserver",
-    "eslint",
-    "luau_lsp",
   },
   -- auto-install configured servers (with lspconfig)
   automatic_installation = true, -- not the same as ensure_installed
