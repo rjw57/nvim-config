5ddea3d7d81f92e22a992546f6ebccd9cc292375
Rich Wareham <rich@richwareham.com>
2022-11-08 21:14:19 +0000

    fix indent

--- lua/plugins-setup.lua
+++ lua/plugins-setup.lua
@@ -1,13 +1,13 @@
 -- auto install packer if not installed
 local ensure_packer = function()
-	local fn = vim.fn
-	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-	if fn.empty(fn.glob(install_path)) > 0 then
-		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
-		vim.cmd([[packadd packer.nvim]])
-		return true
-	end
-	return false
+  local fn = vim.fn
+  local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+    vim.cmd([[packadd packer.nvim]])
+    return true
+  end
+  return false
 end
 local packer_bootstrap = ensure_packer() -- true if packer was just installed
 
@@ -23,77 +23,77 @@ vim.cmd([[
 -- import packer safely
 local status, packer = pcall(require, "packer")
 if not status then
-	return
+  return
 end
 
 -- add list of plugins to install
 return packer.startup(function(use)
-	-- packer can manage itself
-	use("wbthomason/packer.nvim")
+  -- packer can manage itself
+  use("wbthomason/packer.nvim")
 
-	-- lua functions that many plugins use
-	use("nvim-lua/plenary.nvim")
+  -- lua functions that many plugins use
+  use("nvim-lua/plenary.nvim")
 
-	-- colorscheme
-	use("ericbn/vim-solarized")
+  -- colorscheme
+  use("ericbn/vim-solarized")
 
-	-- "Leap" cursor navigation
-	use("ggandor/leap.nvim")
+  -- "Leap" cursor navigation
+  use("ggandor/leap.nvim")
 
-	-- Case aware search and replace
-	use("tpope/vim-abolish")
+  -- Case aware search and replace
+  use("tpope/vim-abolish")
 
-	-- File browser
-	use("nvim-tree/nvim-tree.lua")
+  -- File browser
+  use("nvim-tree/nvim-tree.lua")
 
-	-- Status lune
-	use("nvim-lualine/lualine.nvim")
+  -- Status lune
+  use("nvim-lualine/lualine.nvim")
 
-	-- Autocompletion
-	use("hrsh7th/nvim-cmp")
-	use("hrsh7th/cmp-buffer")
-	use("hrsh7th/cmp-path")
+  -- Autocompletion
+  use("hrsh7th/nvim-cmp")
+  use("hrsh7th/cmp-buffer")
+  use("hrsh7th/cmp-path")
 
-	-- Downloading and configuring LSP servers
-	use("williamboman/mason.nvim")
-	use("williamboman/mason-lspconfig.nvim")
-	use("neovim/nvim-lspconfig")
-	use("hrsh7th/cmp-nvim-lsp")
+  -- Downloading and configuring LSP servers
+  use("williamboman/mason.nvim")
+  use("williamboman/mason-lspconfig.nvim")
+  use("neovim/nvim-lspconfig")
+  use("hrsh7th/cmp-nvim-lsp")
 
-	-- Treesitter parsing
-	use({
-		"nvim-treesitter/nvim-treesitter",
-		run = function()
-			require("nvim-treesitter.install").update({ with_sync = true })
-		end,
-	})
+  -- Treesitter parsing
+  use({
+    "nvim-treesitter/nvim-treesitter",
+    run = function()
+      require("nvim-treesitter.install").update({ with_sync = true })
+    end,
+  })
 
-	-- Git integration
-	use("tpope/vim-fugitive")
+  -- Git integration
+  use("tpope/vim-fugitive")
 
-	-- Telescope for fuzzy search
-	use({
-		"nvim-telescope/telescope.nvim",
-		branch = "0.1.x",
-		requires = { { "nvim-lua/plenary.nvim" } },
-	})
-	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
+  -- Telescope for fuzzy search
+  use({
+    "nvim-telescope/telescope.nvim",
+    branch = "0.1.x",
+    requires = { { "nvim-lua/plenary.nvim" } },
+  })
+  use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
 
-	-- Editorconfig
-	use("editorconfig/editorconfig-vim")
+  -- Editorconfig
+  use("editorconfig/editorconfig-vim")
 
-	-- Markdown
-	use("plasticboy/vim-markdown")
+  -- Markdown
+  use("plasticboy/vim-markdown")
 
-	-- Linting and formatting
-	use("jose-elias-alvarez/null-ls.nvim")
-	use("jayp0521/mason-null-ls.nvim")
+  -- Linting and formatting
+  use("jose-elias-alvarez/null-ls.nvim")
+  use("jayp0521/mason-null-ls.nvim")
 
-	-- Snippets (needed for autocomplete)
-	use("L3MON4D3/LuaSnip") -- snippet engine
-	use("saadparwaiz1/cmp_luasnip") -- for autocompletion
+  -- Snippets (needed for autocomplete)
+  use("L3MON4D3/LuaSnip") -- snippet engine
+  use("saadparwaiz1/cmp_luasnip") -- for autocompletion
 
-	if packer_bootstrap then
-		require("packer").sync()
-	end
+  if packer_bootstrap then
+    require("packer").sync()
+  end
 end)
