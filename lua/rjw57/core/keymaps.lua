cc3ff1908a2cd9b700d945975f2e81d1f83065a4
Rich Wareham <rich@richwareham.com>
2022-11-08 20:26:32 +0000

    support lsp hover

--- lua/rjw57/core/keymaps.lua
+++ lua/rjw57/core/keymaps.lua
@@ -29,3 +29,10 @@ vim.api.nvim_set_keymap(
 vim.api.nvim_set_keymap("n", "<leader>d[", "<cmd>lua vim.diagnostic.goto_prev()<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>d]", "<cmd>lua vim.diagnostic.goto_next()<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>dd", "<cmd>Telescope diagnostics<CR>", { noremap = true, silent = true })
+
+-- LSP
+vim.api.nvim_create_autocmd("LspAttach", {
+	callback = function(args)
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = args.buf })
+	end,
+})
