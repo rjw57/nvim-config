c089a5e40cb9361f3dfb4ddc303b144974cb76f3
Rich Wareham <rich@richwareham.com>
2022-11-08 21:47:07 +0000

    fix indent

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,54 +1,54 @@
 -- import nvim-treesitter plugin safely
 local status, treesitter = pcall(require, "nvim-treesitter.configs")
 if not status then
-	return
+  return
 end
 
 -- configure treesitter
 treesitter.setup({
-	-- enable syntax highlighting
-	highlight = {
-		enable = true,
-		-- treesitter highlighting does not play nicely with vim-markdown
-		disable = { "markdown", "markdown_inline" },
-	},
-	-- enable indentation
-	indent = { enable = true },
-	-- enable autotagging (w/ nvim-ts-autotag plugin)
-	autotag = { enable = true },
-	-- ensure these language parsers are installed
-	ensure_installed = {
-		"bash",
-		"c_sharp",
-		"cmake",
-		"cpp",
-		"css",
-		"dart",
-		"dockerfile",
-		"dot",
-		"gitattributes",
-		"glsl",
-		"go",
-		"gomod",
-		"graphql",
-		"hcl",
-		"html",
-		"java",
-		"javascript",
-		"jsdoc",
-		"json",
-		"latex",
-		"markdown",
-		"markdown_inline",
-		"scss",
-		"sql",
-		"svelte",
-		"toml",
-		"tsx",
-		"typescript",
-		"vim",
-		"yaml",
-	},
-	-- auto install above language parsers
-	auto_install = true,
+  -- enable syntax highlighting
+  highlight = {
+    enable = true,
+    -- treesitter highlighting does not play nicely with vim-markdown
+    disable = { "markdown", "markdown_inline" },
+  },
+  -- enable indentation
+  indent = { enable = true },
+  -- enable autotagging (w/ nvim-ts-autotag plugin)
+  autotag = { enable = true },
+  -- ensure these language parsers are installed
+  ensure_installed = {
+    "bash",
+    "c_sharp",
+    "cmake",
+    "cpp",
+    "css",
+    "dart",
+    "dockerfile",
+    "dot",
+    "gitattributes",
+    "glsl",
+    "go",
+    "gomod",
+    "graphql",
+    "hcl",
+    "html",
+    "java",
+    "javascript",
+    "jsdoc",
+    "json",
+    "latex",
+    "markdown",
+    "markdown_inline",
+    "scss",
+    "sql",
+    "svelte",
+    "toml",
+    "tsx",
+    "typescript",
+    "vim",
+    "yaml",
+  },
+  -- auto install above language parsers
+  auto_install = true,
 })
