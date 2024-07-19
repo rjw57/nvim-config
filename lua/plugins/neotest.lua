cae09e6534680c7f14f7d48e9c247eeda52fb1e1
Rich Wareham <rich@richwareham.com>
2024-07-19 22:18:45 +0100

    fix: add missing file

---
+++ lua/plugins/neotest.lua
@@ -0,0 +1,17 @@
+local status, neotest, neotest_python
+
+status, neotest = pcall(require, "neotest")
+if not status then
+  return
+end
+
+status, neotest_python = pcall(require, "neotest-python")
+if not status then
+  return
+end
+
+neotest.setup({
+  adapters = {
+    neotest_python,
+  }
+})
