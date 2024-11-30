556e9156f4212d4f84b7a7fc4b4b02047fec155c
Rich Wareham <rich@richwareham.com>
2024-11-30 12:56:52 +0000

    feat: let Lazy.vim do the auto installing of mason components

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,28 +2,26 @@ return {
   { "jose-elias-alvarez/null-ls.nvim" },
   {
     "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        pylsp = {
-          settings = {
-            pylsp = {
-              configurationSources = { "pycodestyle", "flake8" },
-              plugins = {
-                pycodestyle = {
-                  enabled = false,
-                },
-                flake8 = {
-                  enabled = true,
-                },
-                pylint = {
-                  enabled = false,
-                },
+    opts = function(_, opts)
+      opts.servers.pylsp = {
+        settings = {
+          pylsp = {
+            configurationSources = { "pycodestyle", "flake8" },
+            plugins = {
+              pycodestyle = {
+                enabled = false,
+              },
+              flake8 = {
+                enabled = true,
+              },
+              pylint = {
+                enabled = false,
               },
             },
           },
         },
-      },
-    },
+      }
+    end,
   },
   { "glepnir/lspsaga.nvim" },
   { "hrsh7th/cmp-nvim-lsp" },
