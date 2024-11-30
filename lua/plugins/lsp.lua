8be02fd4eab1a6bea3a24ee816182c4d6de3c52c
Rich Wareham <rich@richwareham.com>
2024-11-30 08:16:38 +0000

    feat: disable pycodestyle

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,29 +1,29 @@
 return {
+  { "jose-elias-alvarez/null-ls.nvim" },
   {
-    "jose-elias-alvarez/null-ls.nvim",
-    opts = function()
-      local null_ls = require("null-ls")
-      local formatting = null_ls.builtins.formatting -- to setup formatters
-      local diagnostics = null_ls.builtins.diagnostics -- to setup linters
-
-      return {
-        -- setup formatters & linters
-        sources = {
-          --  to disable file types use
-          --  "formatting.prettier.with({disabled_filetypes: {}})" (see null-ls docs)
-          formatting.prettier, -- js/ts formatter
-          formatting.stylua, -- lua formatter
-          formatting.black,
-          formatting.isort,
-          diagnostics.eslint_d.with({ -- js/ts linter
-            -- only enable eslint if root has .eslintrc.js (not in youtube nvim video)
-            condition = function(utils)
-              return utils.root_has_file(".eslintrc.js") -- change file extension if you use something else
-            end,
-          }),
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        pylsp = {
+          settings = {
+            pylsp = {
+              configurationSources = { "pycodestyle", "flake8" },
+              plugins = {
+                pycodestyle = {
+                  enabled = false,
+                },
+                flake8 = {
+                  enabled = true,
+                },
+                pylint = {
+                  enabled = false,
+                },
+              },
+            },
+          },
         },
-      }
-    end,
+      },
+    },
   },
   { "glepnir/lspsaga.nvim" },
   { "hrsh7th/cmp-nvim-lsp" },
--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,17 +1,3 @@
 return {
-  "hrsh7th/nvim-cmp",
-  opts = function()
-    local cmp = require("cmp")
-    return {
-      -- TODO: I'm not super happy with these mappings.
-      mapping = cmp.mapping.preset.insert({
-        ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
-        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-w>"] = cmp.mapping.confirm(),
-        ["<C-p>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-        ["<C-n>"] = cmp.mapping.select_next_item(), -- next suggestion
-      }),
-    }
-  end,
+  { "hrsh7th/nvim-cmp" },
 }
