1d8c37c98cc10bb968611ad4f6e03455a590a0f9
Rich Wareham <rich@richwareham.com>
2025-02-14 11:40:58 +0000

    feat: re-enable Ctrl-P for live grep

---
+++ lua/plugins/fzf-lua.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "ibhagwan/fzf-lua",
+    keys = {
+      -- This is wired into my soul.
+      { "<c-p>", LazyVim.pick("files"), desc = "Find Files (Root Dir)" },
+    },
+  },
+}
