543ead5490203960c91dddd3a17dccdab7eec03d
Rich Wareham <rjw57@cam.ac.uk>
2025-03-19 14:14:30 +0000

    feat: disable ghost text in blink since it sometimes breaks

--- lua/plugins/blink.cmp.lua
+++ lua/plugins/blink.cmp.lua
@@ -1,8 +1,9 @@
 return {
   {
     "saghen/blink.cmp",
-    opts = function (_, opts)
+    opts = function(_, opts)
       opts.completion.accept.auto_brackets.enabled = false
+      opts.completion.ghost_text.enabled = false
       opts.keymap.preset = "default"
       opts.sources.default = { "lsp", "path", "buffer" }
     end,
